9604f20b2c210021060fd3ecc496569ecda003cb
zakumann <samuel742able@gmail.com>
2023-09-01 22:19:34 +0900

    09012023

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe} from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
